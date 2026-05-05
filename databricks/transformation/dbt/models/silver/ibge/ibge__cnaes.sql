b658de31fa54dba298d54d4ac62907b78de24a2c
Alberto Randon <albertonetobrocardo@gmail.com>
2026-05-05 22:52:00 +0100

    fix naming

--- databricks/transformation/dbt/models/silver/ibge/ibge__cnaes.sql
+++ databricks/transformation/dbt/models/silver/ibge/ibge__cnaes.sql
@@ -27,10 +27,10 @@ with base as (
                 >,
                 observacoes:array<string>
             >'
-        ) as classe_struct,
+        ) as class_struct,
 
-        from_json(cast(atividades as string), 'array<string>') as atividades,
-        from_json(cast(observacoes as string), 'array<string>')  as observacoes,
+        from_json(cast(atividades as string), 'array<string>') as activities,
+        from_json(cast(observacoes as string), 'array<string>')  as notes,
 
         cast(_extraction as timestamp) as _extraction,
         cast(_ingestion_ts as timestamp) as _ingestion_ts,
@@ -49,21 +49,21 @@ select
     cnae_id,
     cnae_description,
 
-    trim(classe_struct.id)                                as class_id,
-    trim(classe_struct.descricao)                         as class_description,
-    classe_struct.observacoes                             as class_notes,
+    trim(class_struct.id)                                as class_id,
+    trim(class_struct.descricao)                         as class_description,
+    class_struct.observacoes                             as class_notes,
 
-    trim(classe_struct.grupo.id)                          as group_id,
-    trim(classe_struct.grupo.descricao)                   as group_description,
+    trim(class_struct.grupo.id)                          as group_id,
+    trim(class_struct.grupo.descricao)                   as group_description,
 
-    trim(classe_struct.grupo.divisao.id)                  as division_id,
-    trim(classe_struct.grupo.divisao.descricao)           as division_description,
+    trim(class_struct.grupo.divisao.id)                  as division_id,
+    trim(class_struct.grupo.divisao.descricao)           as division_description,
 
-    trim(classe_struct.grupo.divisao.secao.id)            as section_id,
-    trim(classe_struct.grupo.divisao.secao.descricao)     as section_description,
+    trim(class_struct.grupo.divisao.secao.id)            as section_id,
+    trim(class_struct.grupo.divisao.secao.descricao)     as section_description,
 
-    atividades as cnae_activities,
-    observacoes as cnae_notes,
+    activities as cnae_activities,
+    notes as cnae_notes,
 
     _ingestion_ts,
     _load_ts
