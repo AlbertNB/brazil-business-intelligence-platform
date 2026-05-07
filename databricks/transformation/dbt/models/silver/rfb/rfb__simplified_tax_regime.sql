0142db0cf2a65d5fe50605e7aee26119c7360aaa
Alberto Randon <albertonetobrocardo@gmail.com>
2026-05-07 01:07:49 +0100

    names refactored

--- databricks/transformation/dbt/models/silver/rfb/rfb__simplified_tax_regime.sql
+++ databricks/transformation/dbt/models/silver/rfb/rfb__simplified_tax_regime.sql
@@ -7,9 +7,9 @@ with source as (
 
     select
         trim(cast(_c0 as string))                                       as company_root_id,
-        {{ rfb_flag('_c1') }}                                           as is_simples,
-        {{ rfb_date('_c2') }}                                               as simples_option_date,
-        {{ rfb_date('_c3') }}                                               as simples_exclusion_date,
+        {{ rfb_flag('_c1') }}                                           as is_simples_nacional,
+        {{ rfb_date('_c2') }}                                               as simples_nacional_option_date,
+        {{ rfb_date('_c3') }}                                               as simples_nacional_exclusion_date,
         {{ rfb_flag('_c4') }}                                           as is_mei,
         {{ rfb_date('_c5') }}                                               as mei_option_date,
         {{ rfb_date('_c6') }}                                               as mei_exclusion_date,
@@ -30,9 +30,9 @@ with source as (
 
 select
     company_root_id,
-    is_simples,
-    simples_option_date,
-    simples_exclusion_date,
+    is_simples_nacional,
+    simples_nacional_option_date,
+    simples_nacional_exclusion_date,
     is_mei,
     mei_option_date,
     mei_exclusion_date,
